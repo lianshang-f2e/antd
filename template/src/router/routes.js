93172f1f0a354e8b6757bd4bcbed582f7fa1e9f8
吴茂芳 <maofang.wu@lianshang.com>
2018-10-17 15:19:53 +0800

    fix

--- template/src/router/routes.js
+++ template/src/router/routes.js
@@ -1,11 +1,6 @@
-// import LayoutIndex from '@/Layout/newIndex/index'
 import HomeRoutes from '@/pages/Home/routes'
 import Login from '@/pages/Login/routes'
-// import PackingSlip from '@/pages/PackingSlip/routes'
-// import RequestInquiry from '@/pages/RequestInquiry/routes'
-// import Declaration from '@/pages/Declaration/routes'
 import Demo from '@/pages/Demo/routes'
-// import Order from '@/pages/Order/routes'
 
 const LayoutIndex = () => import('@/Layout/index/index')
 
