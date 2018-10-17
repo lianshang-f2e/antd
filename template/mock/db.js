ba73e4adc78374697e96d3d539da9df66cc19876
吴茂芳 <maofang.wu@lianshang.com>
2018-10-17 17:22:13 +0800

    fix

--- template/mock/db.js
+++ template/mock/db.js
@@ -1,19 +1,7 @@
-const loginApi = require('./api/login.api.js')
-const homeApi = require('./api/home.api.js')
-const requestInquiryApi = require('./api/request-inquiry.api.js')
-const packingSlipApi = require('./api/packing-slip.api.js')
-const tradeApi = require('./api/trade.api.js')
-const userApi = require('./api/user.api.js')
-const IM = require('./api/IM.api.js')
+const demoApi = require('./api/demo.api.js')
 
 let mockApi = {
-  ...loginApi,
-  ...homeApi,
-  ...IM,
-  ...requestInquiryApi,
-  ...userApi,
-  ...packingSlipApi,
-  ...tradeApi,
+  ...demoApi,
   'error': {
     'code': -1,
     'message': 'no such api name'
